f9060ba857b6280f96b7a01ef445133c3d09da27
Yash-practice <yash.khandelwal@nagarro.com>
2024-08-07 13:06:22 +0530

    Update script.sh

--- Chatbot-Codebase-Doc/script.sh
+++ Chatbot-Codebase-Doc/script.sh
@@ -2,12 +2,15 @@
 
 # Print the current date and time
 echo "Starting script at $(date)"
-
-sudo su
-echo "Script in between at $(date)"
+# sudo su
+# gcloud compute ssh vm-genai-llm --zone=us-central1-a
+pwd
+ls -lrt
 cd Medical_Chatbot_Llama2_Pinecone/
+pwd
+ls -lrt
 
-python3 -m streamlit run script.py --server.port 8501
+python3 -m streamlit run script.py --server.port 8501 > streamlit_output.log 2>&1
 
 
 # Print the completion time
