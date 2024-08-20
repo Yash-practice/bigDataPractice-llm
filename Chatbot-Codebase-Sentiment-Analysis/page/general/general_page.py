15e1de99042ac69a2a32b23f5b033bea2f5e69ea
ishatomar <isha.tomar@nagarro.com>
2024-08-20 16:30:06 +0530

    updated ui

--- Chatbot-Codebase-Sentiment-Analysis/page/general/general_page.py
+++ Chatbot-Codebase-Sentiment-Analysis/page/general/general_page.py
@@ -2,14 +2,13 @@ import streamlit as st
 from models import model
 from graphs import graph
 import os
-
-    # Initialize chat history in session state
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
             
 def general_search(domain_name):
+       # Initialize chat history in session state
+    if 'chat_history' not in st.session_state:
+        st.session_state['chat_history'] = []
     st.markdown("""
-        <div class="slider-container">
+    <div class="slider-container">
         <div class="slider">
             <div>😇</div>
             <div>😠</div>
@@ -41,8 +40,7 @@ def general_search(domain_name):
     <h6 class='animated-heading'>🕵️ How may I assist you today?</h6>
     </div>
     """, unsafe_allow_html=True)
-    
- 
+     
     col1, col2 = st.columns([1, 12])
  
     # Empty column for spacing
@@ -57,27 +55,9 @@ def general_search(domain_name):
             st.markdown('</div>', unsafe_allow_html=True)
             st.markdown('<div class="chat-container">', unsafe_allow_html=True)
             
-            # Display existing chat history
-        #     for chat in st.session_state['chat_history']:
-        #         st.markdown(f'<div class="chat-response">User: {chat["user"]}</div>', unsafe_allow_html=True)
-        #         st.markdown(f'<div class="chat-response">Bot: {chat["response"]}</div>', unsafe_allow_html=True)
-        #         if chat.get("graph"):
-        #             st.plotly_chart(chat["graph"], use_container_width=True)
- 
-        #     st.markdown('</div>', unsafe_allow_html=True)
-       
-        # # Input field fixed at the bottom
-        # st.markdown('<div class="fixed-bottom">', unsafe_allow_html=True)
-        # user_input = st.text_input("🤖 Write Some Text Here", "", key="user_input", placeholder="Type your message here...")
-        # st.markdown('</div>', unsafe_allow_html=True)
- 
         model_name = model.domain_model[domain_name][0]
         
-        # # Handle user input and update chat history
-        # if user_input and model_name:
-        #     tokenizer, model_instance = model.load_roberta_model(f'models/{model_name}/model', f'models/{model_name}/tokenizer')
-        #     sentiment_mapping = model_instance.config.id2label
-        #     sentiment = model.predict_sentiment(user_input, model_instance, tokenizer, sentiment_mapping)
+        
         if user_input and model_name:
                 tokenizer, model_instance = model.load_roberta_model(f'models/{model_name}/model', f'models/{model_name}/tokenizer')
                 sentiment_mapping = model_instance.config.id2label
@@ -103,13 +83,13 @@ def general_search(domain_name):
                 gauge_fig = graph.plot_gauge(zones)
  
                 # Add user input, response, and graph to chat history
-                response = f"Response to '{user_input}'"  # Placeholder for actual response
+                response = f"Response to '{user_input}'"  
                 st.session_state['chat_history'].append({
                     'user': user_input,
                     'response': response,
                     'graph': gauge_fig
                 })
- 
+
             # Display new chat entry
                 for chat in reversed(st.session_state['chat_history']):
                     st.markdown(f'<div class="chat-response">User: {chat["user"]}</div>', unsafe_allow_html=True)
