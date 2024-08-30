88776af9f75a92cd4325d25d137001b16407f3a9
gauravpandey-01 <gaurav.pandey01@nagarro.com>
2024-08-30 18:20:21 +0530

    kill the previous process on provided port before starting new process

--- Chatbot-Codebase-Sentiment-Analysis/script.sh
+++ Chatbot-Codebase-Sentiment-Analysis/script.sh
@@ -2,6 +2,25 @@
 
 # Print the current date and time
 echo "Starting script at $(date)"
+
+# Port number to be used
+PORT=8502
+
+# Find the PID of the process using the specified port
+PID=$(lsof -ti :$PORT)
+
+# If no PID is found, exit with a message
+if [ -z "$PID" ]; then
+  echo "No process found using port $PORT."
+  exit 0
+fi
+
+# Kill the process
+kill -9 $PID
+
+# Print confirmation
+echo "Killed process $PID using port $PORT."
+
 # sudo su
 # gcloud compute ssh vm-genai-llm --zone=us-central1-a
 pwd
@@ -12,7 +31,7 @@ ls -lrt
 
 python3 -m pip install -r requirements.txt
 python3 -m spacy download en_core_web_sm
-nohup python3 -m streamlit run script.py --server.port 8502 > streamlit_output.log 2>&1 &
+nohup python3 -m streamlit run script.py --server.port $PORT > streamlit_output.log 2>&1 &
 
 
 # Print the completion time
